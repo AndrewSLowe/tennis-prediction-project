20736d07f41fd65ac864fc1b9e23f3045af51eeb
Andrew Lowe <andrewlowe@Andrews-MacBook-Air.local>
2019-10-23 18:40:33 -0400

    Add existing files

--- pages/predictions.py
+++ pages/predictions.py
@@ -3,26 +3,35 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+import xgboost
 from app import app
+from joblib import load
+
+pipeline = load('assets/pipeline.joblib')
 
 column1 = dbc.Col(
     [
-        dcc.Markdown(
-            """
-        
-            ## Predictions
-
-
-            """
+        dcc.Markdown('## Match up', className='MB-5'),
+        dcc.Markdown('#### Player A'), 
+        dcc.Input(
+            placeholder="Enter a player's name...",
+            type='text',
+            value='',
         ),
+        dcc.Markdown('#### Player B', className='mb-5'), 
+        dcc.Input(
+            placeholder="Enter a player's name...",
+            type='text',
+            value=''
+        ), 
     ],
     md=4,
 )
-
+     
 column2 = dbc.Col(
     [
-        
+        html.H2('Expected Winner', className='mb-5'), 
+        html.Div(id='prediction-content', className='lead')
     ]
 )
 
