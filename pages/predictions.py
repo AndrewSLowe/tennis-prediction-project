be73296be0ac100c6ed13a0dd0078caf898fd34d
Andrew Lowe <andrewlowe@Andrews-MacBook-Air.local>
2019-10-23 23:08:42 -0400

    Almost have prediction working

--- pages/predictions.py
+++ pages/predictions.py
@@ -6,8 +6,10 @@ from dash.dependencies import Input, Output
 import xgboost
 from app import app
 from joblib import load
+import pandas as pd
 
 pipeline = load('assets/pipeline.joblib')
+print('Pipeline loaded!')
 
 column1 = dbc.Col(
     [
@@ -24,6 +26,39 @@ column1 = dbc.Col(
             type='text',
             value=''
         ), 
+        dcc.Markdown('## Break Points Faced', className='mb-5'), 
+        dcc.Markdown('#### Player A'),
+        dcc.Slider(
+            min=0,
+            max=20,
+            step=1,
+            value=3,
+        ),  
+        dcc.Markdown('## Break Points Faced', className='mb-5'), 
+        dcc.Markdown('#### Player B'),
+        dcc.Slider(
+            min=0,
+            max=20,
+            step=1,
+            value=3,
+        ),
+        dcc.Markdown('## Break Points Saved', className='mb-5'), 
+        dcc.Markdown('#### Player A'),
+        dcc.Slider(
+            min=0,
+            max=20,
+            step=1,
+            value=3,
+        ),  
+        dcc.Markdown('## Break Points Saved', className='mb-5'), 
+        dcc.Markdown('#### Player B'),
+        dcc.Slider(
+            min=0,
+            max=20,
+            step=1,
+            value=3,
+        ),  
+
     ],
     md=4,
 )
@@ -36,3 +71,19 @@ column2 = dbc.Col(
 )
 
 layout = dbc.Row([column1, column2])
+
+@app.callback(
+    Output('prediction-content', 'children'),
+    [Input('Player_A', 'value'), Input('Player_B', 'value'),
+    Input('Player_A_bpFaced', 'value'), Input('Player_B_bpFaced', 'value'),
+    Input('Player_A_bpSaved', 'value'), Input('Player_B_bpSaved', 'value')],
+)
+
+def predict(Player_A, Player_B, Player_A_bpFaced, Player_B_bpFaced, Player_A_bpSaved, Player_B_bpSaved):
+    df = pd.DataFrame(
+        columns = ['Player_A', 'Player_B', 'Player_A_bpFaced', 
+             'Player_B_bpFaced','Player_A_bpSaved', 'Player_B_bpSaved'],
+        data=[[Player_A, Player_B, Player_A_bpFaced, Player_B_bpFaced, Player_A_bpSaved, Player_B_bpSaved]]
+    )
+    y_pred = pipeline.predict(df)[0]
+    return y_pred
