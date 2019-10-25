eee41300b31e454639d985c0ae0dcb37156eccc0
Andrew Lowe <andrewlowe@Andrews-MacBook-Air.local>
2019-10-25 13:36:01 -0400

    final commit

--- pages/process.py
+++ pages/process.py
@@ -18,7 +18,7 @@ row1 = dbc.Row(
         dcc.Markdown(
             """
             #### **Project Inspiration**
-            ##### My inspiration for the tennis match outcome predictor was actually from a Lambda student from an earlier Cohort, Scott Huston, who made an NFL team run/pass predictor called "Robo Romo." I thought that if Scott could make a model that would predict whether a team would run or pass, I should be able to predict who would win in a tennis match between two players, given some relevant information about the match and who the players were. Being a tennis fan, I was excited for the challenge.
+            ##### My inspiration for the tennis match outcome predictor was actually from a project done by a Lambda student from an earlier Cohort, Scott Huston. His project, the "Robo Romo", predicts whether an NFL team will run or pass depending on a handful of features. I thought that if Scott could make a model that would predict whether a team would run or pass, I should be able to predict who would win in a tennis match between two players, given some relevant information about the match and who the players were. Being a tennis fan, I was excited for the challenge.
             """,
             className= 'mb-3'
 
@@ -50,8 +50,8 @@ col2 = dbc.Col(
         dcc.Markdown(
             """
             ##### I began by finding a dataset from github.com/JeffSackman who has information from every professional tennis match over the past 50 years. After many hours of combining these dataframes, I was finally ready to do some analysis. 
-            ##### Machine learning models have features (different variables used to predict the target variable; in my case, the target is which player won a match). But half of the variables I didn't even understand, (P.S. If anyone can tell me what "1stWon means", please email me ASAP.) so my goal was to reduce the complexity of my model by getting rid of features that weren't positively influencing my model and that wouldn't be interesting features for a user to use to predict a match. On the right we can see a list of my top feature importances--those that most positively influenced my model's accuracy (a metric that doesn't need to be understood quite yet).
-            ##### I decided just to stick with the player names, how many break points they faced and how many break points they saved.
+            ##### Machine learning models have features (different variables used to predict the target variable; in my case, the target is which player won a match). But I didn't know what half of the variables meant (P.S. If anyone can tell me what "1stWon" means in relation to tennis, please email me ASAP.) so my goal was to reduce the complexity of my model by getting rid of features that weren't positively influencing my model and that wouldn't be interesting features for a user to use to predict a match. On the right we can see a list of my top feature importances--those that most influenced my model's accuracy (and accuracy is a metric that tells us how reliable we are to expect our model to be in predicting the target).
+            ##### I decided just to stick with the player names, how many break points they faced and how many break points they saved, since those are interesting features and have a lot of influence on the accuracy of the model.
             """,
         )
     ],       
@@ -64,8 +64,8 @@ row4 = dbc.Row(
             """
             #### Results
             ##### My baseline (i.e. the prediction one would make without measuring the influence of different features) would be a 50/50. It's a coing flip when you ignore who the players are, their win/loss ratios, their highest ranks, possible match-specific circumstances like breakpoints faced and saved, etc., and just account for the fact that they are two players playing a game where only one player can win. That's a prediction a third grader could make with a little guidance.
-            ##### But once I built a machine learning model my accuracy score went up to 78% test accuracy. The idea is that, given the 50+ features in my machine learning model, that it would predict the winner of a tennis match 78% of the time.
-            ##### These are good results, but I wanted to make my model with features that a user would care about. So I stripped away a lot of the noise (features that had little positive effect on the accuracy score), and obly kept the names of the two players, how many break points they faced, and how many break points they saved. For non-tennis players, a break point is a pivotal moment in a match. So for those who aren't interested in the scoring a rules of tennis but want to use my app anyways, just know what facing a break point is bad for a player, and saving a break point is good for a player. Shaving the rest of these features only brought my accuracy score down by 2 points, to 76%. Still a very strong model that users can now interacy with.
+            ##### But once I built a machine learning model my accuracy score went up to 78% test accuracy. The idea is that, given the 50+ features in my machine learning model, that it would predict the correct winner of a tennis match 78% of the time.
+            ##### These are good results, but I wanted to make my model with features that a user would care about. So I stripped away a lot of the noise (features that had little positive effect on the accuracy score), and only kept the names of the two players, how many break points they faced, and how many break points they saved. For non-tennis players, a break point is a pivotal moment in a match. So for those who aren't interested in the scoring a rules of tennis but want to use my app anyways, just know what facing a break point is bad for a player, and saving a break point is good for a player. Shaving the rest of these features brought my accuracy score down by 4 points, to 74% test accuracy. Still a very strong model that users can now interacy with.
             """
         )
     ]
@@ -76,7 +76,7 @@ row5 = dbc.Row(
             """
             #### Feature Analysis
             ##### Below we see what is called a partial dependency plot of the variable that had the highest impact on my model's accuracy score, "Player_A_bpFaced". This is how many break points player A faced (again, break points are bad for the player who's facing them). 
-            ##### This partial dependency plot is like a more complex picture of what the chart above on permutation importances tells us about this one feature. The chart above tells us that this feature has the most influence on the model of any feature, but we don't know from that in which direction it takes our predictions or the extent at different amount of break points faced. So looking at this graph, we see that break points faced by player A is monotonic (one directional). Furthermore, a the amount of break points that Player A faced increases, odds start to look worse and worse for her chance of winning the match. The influence of Player B break points faced goes in the opposite directions and tells us the same thing for Player B's odds of winning. 
+            ##### This partial dependency plot is like a more complex picture of what the chart above on permutation importances tells us about this one feature. The chart above tells us that this feature has the most influence on the model of any feature, but we don't know from that in which direction it takes our predictions or the extent at different amount of break points faced. So looking at this graph, we see that break points faced by player A is monotonic (one directional). Furthermore, the amount of break points that Player A faced increases, odds start to look worse and worse for her chance of winning the match. The influence of Player B break points faced goes in the opposite directions and tells us the same thing for Player B's odds of winning. 
             """
         )
     ]
@@ -145,7 +145,7 @@ row11 = dbc.Row(
     [
         dcc.Markdown(
             """
-            ##### Finally, lets look at the effect of each feature on a specific match in relation to one another. This shap graph shows the interaction between all of these features in the match. The middle, at value 0, represents our 50/50 baseline, a coin flip as to who we would predict to win the match. But given the Player names, the amount of break points they faced and saved, it predicts rather that 
+            ##### Finally, lets look at the effect of each feature on a specific match in relation to one another. This shap graph shows the interaction between all of these features in the match. The middle, at value 0, represents our 50/50 baseline, a coin flip as to who we would predict to win the match. But given the Player names, the amount of break points they faced and saved, it predicts that Player B, Matthias Bachinger, will win the match.
             """
         )
     ]
@@ -165,4 +165,15 @@ row12 = dbc.Row(
     ]
 )
 
-layout = dbc.Row([row1, row2, row3, row5, row6, row7, row8, row9, row10, row11, row12])
+row13 =dbc.Row(
+    [
+        dcc.Markdown(
+            """
+            #### Word of Caution
+            ##### Although my accuracy score is quite high, I ran an ROC AUC test that gave me about 86%. This means that we expect 86% of the predictions to be true positives and true negatives, but then about 14% will be false positives and false negatives. Whenever one runs a machine learning model such as mine, we always have to chose a threshold that determines the amount of false predictions we are willing to accept. Mine, for this model, says we are willing to accepts about 14% false predicitons.
+            """
+        )
+    ]
+)
+
+layout = dbc.Row([row1, row2, row3, row5, row6, row7, row8, row9, row10, row11, row12, row13])
